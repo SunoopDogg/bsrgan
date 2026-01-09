a160304fcc20414e1374c88b1baea0a350f517a5
SunoopDogg <aswoo55555@gmail.com>
2026-01-09 19:15:32 +0000

    feat: Add CLI args and PSNR/SSIM CSV evaluation to inference script

--- main_test_bsrgan.py
+++ main_test_bsrgan.py
@@ -1,123 +1,91 @@
-import os.path
+import argparse
+import csv
 import logging
+import os.path
+
 import torch
 
-from utils import utils_logger
-from utils import utils_image as util
-# from utils import utils_model
 from models.network_rrdbnet import RRDBNet as net
-
-
-"""
-Spyder (Python 3.6-3.7)
-PyTorch 1.4.0-1.8.1
-Windows 10 or Linux
-Kai Zhang (cskaizhang@gmail.com)
-github: https://github.com/cszn/BSRGAN
-        https://github.com/cszn/KAIR
-If you have any question, please feel free to contact with me.
-Kai Zhang (e-mail: cskaizhang@gmail.com)
-by Kai Zhang ( March/2020 --> March/2021 --> )
-This work was previously submitted to CVPR2021.
-
-# --------------------------------------------
-@inproceedings{zhang2021designing,
-  title={Designing a Practical Degradation Model for Deep Blind Image Super-Resolution},
-  author={Zhang, Kai and Liang, Jingyun and Van Gool, Luc and Timofte, Radu},
-  booktitle={arxiv},
-  year={2021}
-}
-# --------------------------------------------
-
-"""
+from utils import utils_image as util
+from utils import utils_logger
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='BSRGAN', help='model name: BSRGAN, BSRGANx2, BSRNet')
+    parser.add_argument('--input', type=str, default='testsets/RealSRSet', help='input LQ image directory')
+    parser.add_argument('--gt', type=str, default=None, help='GT image directory for PSNR/SSIM evaluation')
+    parser.add_argument('--output', type=str, default=None, help='output directory (default: <input>_results)')
+    parser.add_argument('--csv', type=str, default=None, help='CSV output path (default: <output>/metrics.csv)')
+    args = parser.parse_args()
+
+    model_name = args.model
+    sf = 2 if 'x2' in model_name.lower() else 4
 
     utils_logger.logger_info('blind_sr_log', log_path='blind_sr_log.log')
     logger = logging.getLogger('blind_sr_log')
 
-#    print(torch.__version__)               # pytorch version
-#    print(torch.version.cuda)              # cuda version
-#    print(torch.backends.cudnn.version())  # cudnn version
-
-    testsets = 'testsets'       # fixed, set path of testsets
-    testset_Ls = ['RealSRSet']  # ['RealSRSet','DPED']
-
-    model_names = ['RRDB','ESRGAN','FSSR_DPED','FSSR_JPEG','RealSR_DPED','RealSR_JPEG']
-    model_names = ['BSRGAN']    # 'BSRGANx2' for scale factor 2
-
-
-
-    save_results = True
-    sf = 4
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    for model_name in model_names:
-        if model_name in ['BSRGANx2']:
-            sf = 2
-        model_path = os.path.join('model_zoo', model_name+'.pth')          # set model path
-        logger.info('{:>16s} : {:s}'.format('Model Name', model_name))
+    model_path = os.path.join('model_zoo', model_name + '.pth')
+    logger.info('{:>16s} : {:s}'.format('Model Name', model_name))
 
-        # torch.cuda.set_device(0)      # set GPU ID
-        logger.info('{:>16s} : {:<d}'.format('GPU ID', torch.cuda.current_device()))
-        torch.cuda.empty_cache()
+    model = net(in_nc=3, out_nc=3, nf=64, nb=23, gc=32, sf=sf)
+    model.load_state_dict(torch.load(model_path, weights_only=True), strict=True)
+    model.eval()
+    for _, v in model.named_parameters():
+        v.requires_grad = False
+    model = model.to(device)
 
-        # --------------------------------
-        # define network and load model
-        # --------------------------------
-        model = net(in_nc=3, out_nc=3, nf=64, nb=23, gc=32, sf=sf)  # define network
+    L_path = args.input
+    E_path = args.output if args.output else args.input + f'_results_{model_name}'
+    util.mkdir(E_path)
 
-#            model_old = torch.load(model_path)
-#            state_dict = model.state_dict()
-#            for ((key, param),(key2, param2)) in zip(model_old.items(), state_dict.items()):
-#                state_dict[key2] = param
-#            model.load_state_dict(state_dict, strict=True)
+    logger.info('{:>16s} : {:s}'.format('Input Path', L_path))
+    logger.info('{:>16s} : {:s}'.format('Output Path', E_path))
+    if args.gt:
+        logger.info('{:>16s} : {:s}'.format('GT Path', args.gt))
 
-        model.load_state_dict(torch.load(model_path), strict=True)
-        model.eval()
-        for k, v in model.named_parameters():
-            v.requires_grad = False
-        model = model.to(device)
-        torch.cuda.empty_cache()
+    results = []
 
-        for testset_L in testset_Ls:
+    for idx, img_path in enumerate(util.get_image_paths(L_path), 1):
+        img_name, ext = os.path.splitext(os.path.basename(img_path))
+        logger.info('{:->4d} --> {:<s} --> x{:<d}--> {:<s}'.format(idx, model_name, sf, img_name + ext))
 
-            L_path = os.path.join(testsets, testset_L)
-            #E_path = os.path.join(testsets, testset_L+'_'+model_name)
-            E_path = os.path.join(testsets, testset_L+'_results_x'+str(sf))
-            util.mkdir(E_path)
+        img_L = util.imread_uint(img_path, n_channels=3)
+        img_L = util.uint2tensor4(img_L)
+        img_L = img_L.to(device)
 
-            logger.info('{:>16s} : {:s}'.format('Input Path', L_path))
-            logger.info('{:>16s} : {:s}'.format('Output Path', E_path))
-            idx = 0
+        img_E = model(img_L)
+        img_E = util.tensor2uint(img_E)
 
-            for img in util.get_image_paths(L_path):
+        util.imsave(img_E, os.path.join(E_path, img_name + '_' + model_name + '.png'))
 
-                # --------------------------------
-                # (1) img_L
-                # --------------------------------
-                idx += 1
-                img_name, ext = os.path.splitext(os.path.basename(img))
-                logger.info('{:->4d} --> {:<s} --> x{:<d}--> {:<s}'.format(idx, model_name, sf, img_name+ext))
+        if args.gt:
+            gt_path = os.path.join(args.gt, img_name + ext)
+            if os.path.exists(gt_path):
+                img_GT = util.imread_uint(gt_path, n_channels=3)
+                psnr = util.calculate_psnr(img_E, img_GT)
+                ssim = util.calculate_ssim(img_E, img_GT)
+                logger.info('{:>16s} : {:.4f} dB / {:.4f}'.format('PSNR/SSIM', psnr, ssim))
+                results.append({'filename': img_name + ext, 'psnr': psnr, 'ssim': ssim})
+            else:
+                logger.warning('GT not found: {:s}'.format(gt_path))
 
-                img_L = util.imread_uint(img, n_channels=3)
-                img_L = util.uint2tensor4(img_L)
-                img_L = img_L.to(device)
+    if results:
+        csv_path = args.csv if args.csv else os.path.join(E_path, 'metrics.csv')
+        with open(csv_path, 'w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=['filename', 'psnr', 'ssim'])
+            writer.writeheader()
+            writer.writerows(results)
 
-                # --------------------------------
-                # (2) inference
-                # --------------------------------
-                img_E = model(img_L)
+            avg_psnr = sum(r['psnr'] for r in results) / len(results)
+            avg_ssim = sum(r['ssim'] for r in results) / len(results)
+            writer.writerow({'filename': 'average', 'psnr': avg_psnr, 'ssim': avg_ssim})
 
-                # --------------------------------
-                # (3) img_E
-                # --------------------------------
-                img_E = util.tensor2uint(img_E)
-                if save_results:
-                    util.imsave(img_E, os.path.join(E_path, img_name+'_'+model_name+'.png'))
+        logger.info('Average PSNR: {:.4f} dB, SSIM: {:.4f}'.format(avg_psnr, avg_ssim))
+        logger.info('CSV saved to: {:s}'.format(csv_path))
 
 
 if __name__ == '__main__':
-
     main()
